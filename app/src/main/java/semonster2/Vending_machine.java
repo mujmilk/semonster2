5ac79b6ac20fe0586ca6998f0c4ed062b5b3214e
machi0303 <pinto081201@gmail.com>
2022-07-10 04:12:34 +0900

    ルーレットをランダムで当たりを決めるのではなく下一桁が１の時に当たるように変更　buyとrouletteのテストコードの大枠を作成 その他雑多修正

--- app/src/main/java/semonster2/Vending_machine.java
+++ app/src/main/java/semonster2/Vending_machine.java
@@ -1,45 +1,40 @@
 package semonster2;
 
-import java.util.Random;
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Vending_machine {
+  public Beverage[] beverage;
 
-  public static String[] buy(int n, int money) {
+  Vending_machine() {
+    this.beverage[0] = new Beverage(100, "いろはす", 0);
+    this.beverage[1] = new Beverage(200, "梅よろし", 1);
+    this.beverage[2] = new Beverage(300, "コーラ", 2);
+  }
+
+  public static String[] buy(int n, int money, Vending_machine vend) {
     String[] result = new String[2];
     int change;
 
-    if(vend.beverage[n].price > money || vend.beverage[n].stock == 0 || vend.beverage.length < n || n < 0){
-      //買えない
+    if (vend.beverage[n].price > money || vend.beverage[n].stock == 0 || vend.beverage.length < n || n < 0) {
+      // 買えない
       change = money;
-      result[0] = "False" + ":" + "N" + ":" + Arrays.toString(change);
-    }else{
-      vend.beverage[n].stock--;//在庫を減らす
-      change = money - vend.beverage[n].price//moneyからnの値段文ひいておつり
-      result[1] = roulette();
-      result[0] = "True" + ":" + vend.beverage[n].name + ":" + Arrays.toString(change);
+      result[0] = "False" + ":" + "N" + ":" + String.valueOf(change);
+    } else {
+      vend.beverage[n].stock--;// 在庫を減らす
+      change = money - vend.beverage[n].price;// moneyからnの値段文ひいておつり
+      result[1] = roulette(n, vend);
+      result[0] = "True" + ":" + vend.beverage[n].name + ":" + String.valueOf(change);
     }
     return result;
   }
 
-  public static String roulette() {
+  public static String roulette(int n, Vending_machine vend) {
     Scanner scanner = new Scanner(System.in);
 
-    Random rand = new Random();
-    int num_1 = rand.nextInt(9);
-    int num_2 = rand.nextInt(9);
     String result;
 
-    for (int i = 0; i < 4; i++) {
-      if (i == 3) {
-        System.out.println(num_2);
-      } else {
-        System.out.print(num_1);
-      }
-    }
-
-    if (num_1 == num_2) {
+    if (n % 10 == 1) {
+      System.out.println("7777");
       System.out.println("当たり，好きなやつをもう一本えらんで");
       int drink_num = scanner.nextInt();
       while (drink_num < 0 || vend.beverage.length < drink_num) {
@@ -51,13 +46,14 @@ public class Vending_machine {
         drink_num = scanner.nextInt();
       }
       vend.beverage[drink_num].stock--;// 在庫を減らす
-      result = "True" + ":" + Arrays.toString(drink_num);
+      result = "True" + ":" + String.valueOf(drink_num);
 
       // drink_numの在庫を減らす処理
     } else {
       System.out.println("はずれ～～～～");
       result = "False" + ":" + "N";
     }
+    scanner.close();
 
     return result;
   }
--- app/src/test/java/semonster2/Vending_machineTest.java
+++ app/src/test/java/semonster2/Vending_machineTest.java
@@ -1,18 +1,21 @@
 package semonster2;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Vending_machineTest {
   @Test
   public void testbuy() {
-    Calculate calculate = new Calculate();
-    int expected = 5;
-    assertEquals(expected, calculate.sum(2, 3));
+    Vending_machine vend = new Vending_machine();
+    String[] expected = new String[2];
+    expected[0] = assertEquals(expected, Vending_machine.buy());
   }
 
   @Test
   public void testroulette() {
-    Calculate calculate = new Calculate();
-    double expected = 2.5;
-    assertEquals(expected, calculate.ave(2, 3), 0.00001);
+    Vending_machine vend = new Vending_machine();
+    String expected = 2.5;
+    assertEquals(expected, Vending_machine.roulette());
   }
 
 }
